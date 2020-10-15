2546106b3a743f70c423c04217debeb22a1cefb1
Francisco Villar Salvador <franvlive@gmail.com>
2020-10-15 20:20:56 +0200

    App - Add header component & add styles

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import styles from './app.module.scss';
 
+import Header from '../header/';
+
 function App() {
   return (
     <section className={styles.main}>
-      
+      <Header />
     </section>
   );
 }
