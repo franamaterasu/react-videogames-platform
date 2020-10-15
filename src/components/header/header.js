cf5cd12bf646556f0a744d4bb6070036337fd48e
Francisco Villar Salvador <franvlive@gmail.com>
2020-10-15 20:20:29 +0200

    Header - Add styles and import in app component

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -3,7 +3,10 @@ import styles from './header.module.scss';
 
 const Header = () => {
     return(
-        <header className={styles.container}></header>
+        <header className={styles.container}>
+            <span className={styles.logo}>ReactBlog</span>
+            <input className={styles.search} type="text" placeholder="Search news..." />
+        </header>
     )
 };
 
